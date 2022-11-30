12af4475020efe97f0e289f2d53ef09e72aeba2b
Luboš Pokorný <zoido@users.noreply.github.com>
2022-11-30 11:52:28 +0100

    Implement logic for determining the URL of the discovery service.
    PART OF https://github.com/h2oai/cloud-discovery/issues/37

---
+++ src/h2o_discovery/discover.py
@@ -0,0 +1,38 @@
+import os
+from typing import Optional
+
+_WELL_KNOWN_PATH = ".ai.h2o.cloud.discovery"
+
+
+def discover_uri(
+    environment: Optional[str] = None, discovery_address: Optional[str] = None
+) -> str:
+    """Uses passed parameters and environment variables to get the uri of the discovery
+    service.
+    """
+    if discovery_address is not None and environment is not None:
+        raise ValueError("cannot specify both discovery and environment")
+
+    if discovery_address is not None:
+        return discovery_address
+
+    if environment is not None:
+        return _discover_uri_from_environment(environment)
+
+    discovery_address = os.environ.get("H2O_CLOUD_DISCOVERY")
+    if discovery_address is not None:
+        return discovery_address
+
+    environment = os.environ.get("H2O_CLOUD_ENVIRONMENT")
+    if environment is not None:
+        return _discover_uri_from_environment(environment)
+
+    raise LookupError(
+        "Cannot determine discovery URI."
+        " Please set H2O_CLOUD_ENVIRONMENT or H2O_CLOUD_DISCOVERY environment variables"
+        "or use the environment or discovery parameters."
+    )
+
+
+def _discover_uri_from_environment(environment):
+    return f"{environment}/{_WELL_KNOWN_PATH}"
