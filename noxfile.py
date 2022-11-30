5bc7ce364572fe7a830f2afe6a849b647b185849
Luboš Pokorný <zoido@users.noreply.github.com>
2022-11-30 10:44:47 +0100

    fix lint

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,9 @@ import nox_poetry
 
 
 @nox_poetry.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
-@nox.parametrize("httpx_version_constraint", ["==0.16.*", "==0.21.*", "==0.22.*", "==0.23.*", ""])
+@nox.parametrize(
+    "httpx_version_constraint", ["==0.16.*", "==0.21.*", "==0.22.*", "==0.23.*", ""]
+)
 def tests(session, httpx_version_constraint):
     session.install("pytest", "pytest-asyncio")
     session.poetry.session.install(f"httpx{httpx_version_constraint}", "respx", ".")
