d84114f793c75cb13653a50f54f0f2103ad6c0e8
Luboš Pokorný <zoido@users.noreply.github.com>
2022-12-02 11:19:57 +0100

    Introduce dataclasses for defined Discovery messages. (#2)
    * Intorduce dataclasses for defined Discovery messages.
    
     - uses `dataclasses` from the standard lib to minimize 3rd dependencies
     - all classes has factory class method that creates new instance from JSON represantation dict that the `grpc-gateway` produces.
    
    PART OF https://github.com/h2oai/cloud-discovery/issues/37
    
    * REVIEW: Add comments for Environment class.
    
    * Update src/h2o_discovery/model.py
    
    Co-authored-by: Jan Gamec <jan.gamec@h2o.ai>
    
    * REVIEW: rename factory methods to `from_json_dict`
    
    Co-authored-by: Jan Gamec <jan.gamec@h2o.ai>

---
+++ src/h2o_discovery/model.py
@@ -0,0 +1,98 @@
+import dataclasses
+from typing import Mapping
+from typing import Optional
+
+
+@dataclasses.dataclass(frozen=True)
+class Service:
+    """Internal representation of a registered service record."""
+
+    # Name of the Service. For example: "services/my-service-name".
+    name: str
+
+    # Name of the Service that can be displayed on the front-end.
+    display_name: str
+
+    # URI for accessing the Service.
+    uri: str
+
+    # Version of the service. Can be the version of the API or the version of
+    # the service. Clients can utilize this information change their behavior
+    # in accessing the service or downloading the correct client version.
+    version: Optional[str]
+
+    # OAuth 2.0 Scope required to access the service. Clients request the
+    # access token with this scope in order to access the service. If the scope
+    # is not defined, clients should use h2o_cloud_platform_scope.
+    oauth2_scope: Optional[str]
+
+    # Requirement Specifier (PEP 508) for the Python client that can be used
+    # for accessing the service.
+    # Any string that can be `pip install`ed.
+    #
+    # Example: my-client==0.1.0
+    python_client: Optional[str]
+
+    @classmethod
+    def from_json_dict(cls, json: Mapping[str, str]) -> "Service":
+        """Create a Service from a JSON dict returned by the server."""
+        return cls(
+            name=json["name"],
+            display_name=json["displayName"],
+            uri=json["uri"],
+            version=json.get("version"),
+            oauth2_scope=json.get("oauth2Scope"),
+            python_client=json.get("pythonClient"),
+        )
+
+
+@dataclasses.dataclass(frozen=True)
+class Client:
+    """Internal representation of a registered client record."""
+
+    # Name of the Client. For example: "clients/h2o-public-client".
+    name: str
+
+    # Name of the Client that can be displayed on the front-end.
+    display_name: str
+
+    # Public OAuth 2.0 client ID that the client needs to use to identify
+    # itself with the IDP.
+    oauth2_client_id: str
+
+    @classmethod
+    def from_json_dict(cls, json: Mapping[str, str]) -> "Client":
+        """Create a Client from a JSON dict returned by the server."""
+        return cls(
+            name=json["name"],
+            display_name=json["displayName"],
+            oauth2_client_id=json["oauth2ClientId"],
+        )
+
+
+@dataclasses.dataclass(frozen=True)
+class Environment:
+    """Internal representation of the environment."""
+
+    # Identifier of the environment. For example: "https://cloud.h2o.ai".
+    # This is the base URL of the environment. Clients can use this to validate
+    # that they are talking to the correct environment.
+    h2o_cloud_environment: str
+
+    # OpenID Connect issuer_url. This is where clients find the OpenID Connect discovery
+    # on the well-known endpoint.
+    issuer_url: str
+
+    # OAuth 2.0 scope that clients should use to access the H2O Cloud Platform.
+    # This is the default scope that clients should use if the service does not
+    # define its own scope.
+    h2o_cloud_platform_oauth2_scope: str
+
+    @classmethod
+    def from_json_dict(cls, json: Mapping[str, str]) -> "Environment":
+        """Create an Environment from a JSON dict returned by the server."""
+        return cls(
+            h2o_cloud_environment=json["h2oCloudEnvironment"],
+            issuer_url=json["issuerUrl"],
+            h2o_cloud_platform_oauth2_scope=json["h2oCloudPlatformOauth2Scope"],
+        )
---
+++ tests/test_model.py
@@ -0,0 +1,64 @@
+from h2o_discovery import model
+
+
+def test_service_from_json_dict():
+    # Given
+    json = {
+        "name": "services/test-service",
+        "displayName": "Test Service",
+        "uri": "http://test-service.domain:1234",
+        "version": "1.0.0",
+        "oauth2Scope": "test-service-scope",
+        "pythonClient": "test-client==1.0.0",
+    }
+
+    # When
+    result = model.Service.from_json_dict(json)
+
+    # Then
+    assert result == model.Service(
+        name="services/test-service",
+        display_name="Test Service",
+        uri="http://test-service.domain:1234",
+        version="1.0.0",
+        oauth2_scope="test-service-scope",
+        python_client="test-client==1.0.0",
+    )
+
+
+def test_client_from_json_dict():
+    # Given
+    json = {
+        "name": "test-client",
+        "displayName": "Test Client",
+        "oauth2ClientId": "test-client-id",
+    }
+
+    # When
+    result = model.Client.from_json_dict(json)
+
+    # Then
+    assert result == model.Client(
+        name="test-client",
+        display_name="Test Client",
+        oauth2_client_id="test-client-id",
+    )
+
+
+def test_environment_from_json_dict():
+    # Given
+    json = {
+        "h2oCloudEnvironment": "https://test.h2o.ai",
+        "issuerUrl": "https://test.h2o.ai",
+        "h2oCloudPlatformOauth2Scope": "test-platform-scope",
+    }
+
+    # When
+    result = model.Environment.from_json_dict(json)
+
+    # Then
+    assert result == model.Environment(
+        h2o_cloud_environment="https://test.h2o.ai",
+        issuer_url="https://test.h2o.ai",
+        h2o_cloud_platform_oauth2_scope="test-platform-scope",
+    )
