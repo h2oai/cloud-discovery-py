ec27d7ec3cf0299a7ba3491af856f692f7b58d09
Luboš Pokorný <zoido@users.noreply.github.com>
2023-02-06 16:13:13 +0100

    refactor: Prepare code for inclusion for sycnhrnous entry point (#18)

--- src/h2o_discovery/client.py
+++ src/h2o_discovery/client.py
@@ -1,70 +1,18 @@
-from typing import List
-from typing import Optional
-
-import httpx
-
-from h2o_discovery import model
+import urllib.parse
 
 
-class Client:
-    """Implementation of the Discovery Service API.
-
-    Listing methods do pagination and always return all of the available objects.
-    """
-
-    def __init__(self, uri: str):
-        self._uri = uri
-
-    async def get_environment(self) -> model.Environment:
-        """Returns the information about the environment."""
-        async with httpx.AsyncClient() as client:
-            resp = await _fetch(client, self._uri + "/v1/environment")
-            return model.Environment.from_json_dict(resp.json()["environment"])
-
-    async def list_services(self) -> List[model.Service]:
-        """Returns the list of all registered services."""
-        async with httpx.AsyncClient() as client:
-            services: List[model.Service] = []
-
-            pages = await _get_all_pages(client, self._uri + "/v1/services")
-            for page in pages:
-                services.extend(
-                    [model.Service.from_json_dict(d) for d in page.get("services", [])]
-                )
-            return services
-
-    async def list_clients(self) -> List[model.Client]:
-        """Returns the list of all registered clients."""
-        async with httpx.AsyncClient() as client:
-            clients: List[model.Client] = []
-
-            pages = await _get_all_pages(client, self._uri + "/v1/clients")
-            for page in pages:
-                clients.extend(
-                    [model.Client.from_json_dict(d) for d in page.get("clients", [])]
-                )
-            return clients
+ENVIRONMENT_ENDPOINT = "/v1/environment"
+SERVICES_ENDPOINT = "/v1/services"
+CLIENTS_ENDPOINT = "/v1/clients"
 
 
-async def _fetch(
-    client: httpx.AsyncClient, uri: str, next_page_token: Optional[str] = None
-) -> httpx.Response:
-    params = None
-    if next_page_token is not None:
-        params = {"pageToken": next_page_token}
-    resp = await client.get(uri, params=params)
-    resp.raise_for_status()
-    return resp
+def get_environment_uri(uri: str) -> str:
+    return urllib.parse.urljoin(uri, ENVIRONMENT_ENDPOINT)
 
 
-async def _get_all_pages(client: httpx.AsyncClient, uri: str) -> List[dict]:
-    next_page_token: Optional[str] = None
-    all_pages: List[dict] = []
+def get_services_uri(uri: str) -> str:
+    return urllib.parse.urljoin(uri, SERVICES_ENDPOINT)
 
-    while True:
-        resp = await _fetch(client, uri, next_page_token)
-        resp_json = resp.json()
-        all_pages.append(resp_json)
-        next_page_token = resp_json.get("nextPageToken")
-        if next_page_token is None:
-            return all_pages
+
+def get_clients_uri(uri: str) -> str:
+    return urllib.parse.urljoin(uri, CLIENTS_ENDPOINT)
