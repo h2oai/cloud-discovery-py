ea7a9ce792158bab9da4ad400f7b1c2cf6f0ba5d
Luboš Pokorný <zoido@users.noreply.github.com>
2023-08-30 10:57:37 +0200

    feat: ✨ add support for reading clientcredentials form the local H2O CLI config
     - implment config path lookusp
     - implement config loading
     - extend credentials loading to accept tokens loaded from the config
     - extend main discover functions to accept config path and utilize
    
    RESOLVES https://github.com/h2oai/cloud-discovery/issues/437

--- src/h2o_discovery/__init__.py
+++ src/h2o_discovery/__init__.py
@@ -1,8 +1,11 @@
 import dataclasses
+import os
 from typing import Optional
+from typing import Union
 
 from h2o_discovery import model
 from h2o_discovery._internal import client
+from h2o_discovery._internal import config
 from h2o_discovery._internal import load
 from h2o_discovery._internal import lookup
 from h2o_discovery._version import __version__  # noqa: F401
@@ -11,44 +14,79 @@ Discovery = model.Discovery
 
 
 def discover(
-    environment: Optional[str] = None, discovery_address: Optional[str] = None
+    environment: Optional[str] = None,
+    discovery_address: Optional[str] = None,
+    config_path: Optional[Union[str, bytes, os.PathLike]] = None,
 ) -> Discovery:
     """Obtains and returns a Discovery object from the discovery service.
 
-    Both arguments are optional. If neither is provided, the environment variable
-    H2O_CLOUD_ENVIRONMENT is used. If that is not set, the environment variable
-    H2O_CLOUD_DISCOVERY is used. If that is not set, a LookupError is raised.
+    All arguments are optional. Discovery determined with the following precedence:
+        - discovery_address parameter
+        - H2O_CLOUD_DISCOVERY environment variable
+        - environment parameter
+        - H2O_CLOUD_ENVIRONMENT environment variable
+
+    Config path is determined with the following precedence:
+        - config_path parameter
+        - H2OCONFIG environment variable
+        - default H2O CLI configuration configuration path
+        - if default path does not exist, no config is loaded
 
     Args:
         environment: The H2O Cloud environment URL to use (e.g. https://cloud.h2o.ai).
         discovery_address: The address of the discovery service.
+        config_path: The path to the H2O CLI configuration file. Only used for
+            credentials lookup.
 
+    Raises:
+        LookupError: If the URI cannot be determined.
     """
     uri = lookup.determine_uri(environment, discovery_address)
+    cfg = config.load_config(lookup.determine_local_config_path(config_path))
 
     discovery = load.load_discovery(client.Client(uri))
-    credentials = load.load_credentials(clients=discovery.clients)
+    credentials = load.load_credentials(
+        clients=discovery.clients, config_tokens=cfg.tokens
+    )
 
     return dataclasses.replace(discovery, credentials=credentials)
 
 
 async def discover_async(
-    environment: Optional[str] = None, discovery_address: Optional[str] = None
+    environment: Optional[str] = None,
+    discovery_address: Optional[str] = None,
+    config_path: Optional[Union[str, bytes, os.PathLike]] = None,
 ) -> Discovery:
     """Obtains and returns a Discovery object from the discovery service.
 
-    Both arguments are optional. If neither is provided, the environment variable
-    H2O_CLOUD_ENVIRONMENT is used. If that is not set, the environment variable
-    H2O_CLOUD_DISCOVERY is used. If that is not set, a LookupError is raised.
+    All arguments are optional. Discovery determined with the following precedence:
+        - discovery_address parameter
+        - H2O_CLOUD_DISCOVERY environment variable
+        - environment parameter
+        - H2O_CLOUD_ENVIRONMENT environment variable
+
+    Config path is determined with the following precedence:
+        - config_path parameter
+        - H2OCONFIG environment variable
+        - default H2O CLI configuration configuration path
+        - if default path does not exist, no config is loaded
 
     Args:
         environment: The H2O Cloud environment URL to use (e.g. https://cloud.h2o.ai).
         discovery_address: The address of the discovery service.
+        config_path: The path to the H2O CLI configuration file. Only used for
+            credentials lookup.
 
+    Raises:
+        LookupError: If the URI cannot be determined.
     """
+
     uri = lookup.determine_uri(environment, discovery_address)
+    cfg = config.load_config(lookup.determine_local_config_path(config_path))
 
     discovery = await load.load_discovery_async(client.AsyncClient(uri))
-    credentials = load.load_credentials(clients=discovery.clients)
+    credentials = load.load_credentials(
+        clients=discovery.clients, config_tokens=cfg.tokens
+    )
 
     return dataclasses.replace(discovery, credentials=credentials)
--- src/h2o_discovery/_internal/lookup.py
+++ src/h2o_discovery/_internal/lookup.py
@@ -1,8 +1,10 @@
 import os
 from typing import Optional
+from typing import Union
 import urllib.parse
 
 _WELL_KNOWN_PATH = ".ai.h2o.cloud.discovery"
+_DEFAULT_LOCAL_CONFIG_PATH = "~/.h2oai/h2o-cli-config.toml"
 
 
 def determine_uri(
@@ -37,3 +39,22 @@ def determine_uri(
 
 def _discovery_uri_from_environment(environment: str):
     return urllib.parse.urljoin(environment + "/", _WELL_KNOWN_PATH)
+
+
+def determine_local_config_path(
+    config_path: Optional[Optional[Union[str, bytes, os.PathLike]]] = None
+) -> Optional[str]:
+    """Uses passed parameter, environment variable and H2O CLI default to get the
+    path to the local config file.
+    """
+    if config_path is not None:
+        return str(os.fspath(config_path))
+
+    config_path = os.environ.get("H2OCONFIG")
+    if config_path is not None:
+        return config_path
+
+    local_config_path = os.path.expanduser(_DEFAULT_LOCAL_CONFIG_PATH)
+    if not os.path.isfile(local_config_path):
+        return None
+    return local_config_path
